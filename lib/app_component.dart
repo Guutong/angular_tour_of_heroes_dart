02f05d1d90bcd620aa1007c7850581d6a688317b
Pornmongkon Pongsai <pornmongkonp@gmail.com>
2018-08-10 17:25:09 +0700

    Add services

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,22 +1,31 @@
+import 'dart:async';
+
 import 'package:angular/angular.dart';
 import 'src/hero.dart';
 import 'src/mock_heroes.dart';
 import 'src/hero_component.dart';
+import 'src/hero_service.dart';
 
 @Component(
-  selector: 'my-app',
-  templateUrl: 'app_component.html',
-  styleUrls: ['app_component.css'],
-  directives: [coreDirectives, HeroComponent]
-)
-class AppComponent {
+    selector: 'my-app',
+    templateUrl: 'app_component.html',
+    styleUrls: ['app_component.css'],
+    directives: [coreDirectives, HeroComponent],
+    providers: [ClassProvider(HeroService)])
+class AppComponent implements OnInit {
   final name = 'Angular';
   final title = 'Tour of Heroes';
   Hero selected;
 
   Hero hero = Hero(1, 'Windstorm');
-  List<Hero> heroes = mockHeroes;
+  List<Hero> heroes;
+  final HeroService _heroService;
+  AppComponent(this._heroService);
 
+  void ngOnInit() => _getHeroes();
+  Future<void> _getHeroes() async {
+    heroes = await _heroService.getAllSlowly();
+  } 
   void onSelect(Hero hero) => selected = hero;
 
 }
